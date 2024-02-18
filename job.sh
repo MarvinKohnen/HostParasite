65222470dceedad8fa6471abdafa953a0993b1db
Jaime Anaya-rojas <jaime.anaya-rojas@uni-muenster.de>
2024-02-18 17:04:27 +0100

    add jobs

--- job.sh
+++ job.sh
@@ -2,13 +2,13 @@
  
 #SBATCH --nodes=1                   # the number of nodes you want to reserve
 #SBATCH --ntasks-per-node=1         # the number of tasks/processes per node
-#SBATCH --cpus-per-task=4          # the number cpus per task
+#SBATCH --cpus-per-task=30          # the number cpus per task
 #SBATCH --partition=long          # on which partition to submit the job
-#SBATCH --time=5:00:00             # the max wallclock time (time limit your job will run)
+#SBATCH --time=24:00:00             # the max wallclock time (time limit your job will run)
  
 #SBATCH --job-name=My_Copepod         # the name of your job
 #SBATCH --mail-type=ALL             # receive an email when your job starts, finishes normally or is aborted
-#SBATCH --mail-user=janayaro@uni-muenster.de # your mail address
+#SBATCH --mail-user=m_kohn03@uni-muenster.de # your mail address
 
 # load needed modules
 ml palma/2022a
@@ -18,4 +18,4 @@ ml Julia/1.8.2-linux-x86_64
 # export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
  
 # run the application
-julia  /home/j/janayaro/Projects_JM/HostParasite/CopepodParasite2D.jl
+julia  /home/j/janayaro/Marvin/HostParasite/CopepodParasite2D.jl
